33823e97ec9f3bd86b92f2b7861cfc4ea96bf18c
Omar Khatib <omar@omarkhatib.co>
2020-01-28 22:32:40 +0100

    check if it work

--- app/src/Controllers/Web/TestController.php
+++ app/src/Controllers/Web/TestController.php
@@ -14,22 +14,23 @@ class TestController
     $response  = wp_remote_get( 'https://openexchangerates.org/api/latest.json?app_id=ed7245e0b2e2444a8b7a6494c9d7fea3');
     $apiData =json_decode($response['body'], true);
 
+    if ($responseBody && $responseBody['error']) {
+      return;
+    }
     
+    
+
     $currency = ['eur','sar','aed','kwd','omr','qar','bhd','jor','dzd','yer','gbp','chf','chf','cad','aud','cny','rub'];
 
+    $try = $apiData['rates']['TRY'];
     $data;
 
+    $data['usd'] = 1 / $try;
+
     foreach ($currency as $curr) {
-      $data[$curr] = 1;
+      $data[$curr] = $apiData['rates'][strtoupper($curr)]  / $try;
     }
-
-
-
-
     
-
-
-		
 
 
 		return \WPEmerge\json($apiData);
