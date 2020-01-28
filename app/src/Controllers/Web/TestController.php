be1656868d46fd82d18cae9f7c42a807f0bc6de0
Omar Khatib <omar@omarkhatib.co>
2020-01-28 22:36:14 +0100

    fix error

--- app/src/Controllers/Web/TestController.php
+++ app/src/Controllers/Web/TestController.php
@@ -14,7 +14,7 @@ class TestController
     $response  = wp_remote_get( 'https://openexchangerates.org/api/latest.json?app_id=ed7245e0b2e2444a8b7a6494c9d7fea3');
     $apiData =json_decode($response['body'], true);
 
-    if ($apiData && $apiData['error']) {
+    if ($apiData && isset($apiData['error'])) {
       return;
     }
     
