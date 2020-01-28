77849df2afa7a19dd9129cdcccf1c2fb9e2ff97d
Omar Khatib <omar@omarkhatib.co>
2020-01-28 22:27:36 +0100

    fix api data

--- app/src/Controllers/Web/TestController.php
+++ app/src/Controllers/Web/TestController.php
@@ -11,7 +11,10 @@ class TestController
 	public function index(Request $request, $view)
 	{
 
-    $apiData  = wp_remote_get( 'https://openexchangerates.org/api/latest.json?app_id=ed7245e0b2e2444a8b7a6494c9d7fea3');
+    $response  = wp_remote_get( 'https://openexchangerates.org/api/latest.json?app_id=ed7245e0b2e2444a8b7a6494c9d7fea3');
+    $apiData =json_decode($response['body'], true);
+
+    
     $currency = ['eur','sar','aed','kwd','omr','qar','bhd','jor','dzd','yer','gbp','chf','chf','cad','aud','cny','rub'];
 
     $data;
