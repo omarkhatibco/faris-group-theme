a4d521f91c36c9b691bcdd713504998ab52689bc
Omar Khatib <omar@omarkhatib.co>
2020-02-26 00:58:52 +0100

    remove salons from aggregation

--- app/src/Controllers/Web/AggregationController.php
+++ app/src/Controllers/Web/AggregationController.php
@@ -17,8 +17,7 @@ class AggregationController
 			$apartments = carbon_get_post_meta( $id, 'appartments' );
     	$prices = array_map(function ($obj) { return $obj['price']; }, $apartments);
 			$sizes = array_map(function ($obj) {return $obj['min_size'];}, $apartments);
-			$rooms = array_map(function ($obj) {return $obj['rooms_count'];}, $apartments);
-			$salons = array_map(function ($obj) {return $obj['salons_count'];}, $apartments);
+			$rooms = array_map(function ($obj) {return $obj['rooms_count'] + $obj['salons_count'];}, $apartments);
 			
       return [
 		 		'min_price' => intval(min($prices)),
@@ -27,8 +26,6 @@ class AggregationController
 				'max_size'  => intval(max($sizes)),
 				'min_rooms'  => intval(min($rooms)),
 				'max_rooms'  => intval(max($rooms)),
-				'min_salons'  => intval(min($salons)),
-				'max_salons'  => intval(max($salons)),
 				
 			];
 		};
@@ -47,8 +44,6 @@ class AggregationController
       'max_size'  => intval(max(array_map(function ($obj) { return $obj['max_size']; }, $data))),
       'min_rooms'  => intval(min(array_map(function ($obj) { return $obj['min_rooms']; }, $data))),
       'max_rooms'  => intval(max(array_map(function ($obj) { return $obj['max_rooms']; }, $data))),
-      'min_salons'  => intval(min(array_map(function ($obj) { return $obj['min_salons']; }, $data))),
-      'max_salons'  => intval(max(array_map(function ($obj) { return $obj['max_salons']; }, $data))),
 		];
 
 		return \WPEmerge\json($data);
