3b0cda8f7130c1181470933722b397181462be76
Omar Khatib <omar@omarkhatib.co>
2020-03-05 00:56:10 +0100

    fix contact

--- app/src/Controllers/Web/ConfigController.php
+++ app/src/Controllers/Web/ConfigController.php
@@ -60,9 +60,10 @@ class ConfigController
 		];
 
 		if (in_array($origin, $allowed_domains)) {
+				header('Access-control-allow-methods: POST');
 				header('Access-Control-Allow-Origin: ' . $origin);
 		}
-		
+
 		return \WPEmerge\json($config);
 	}
 
